a1d8b5b861b7144d9367d88579d0d019c430c6d4
CrsOystr <nicmetz@jackmoves.com>
2016-12-01 19:58:18 -0700

    parsing lines and breaking everything down. Going to try iterating over URLs different

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MiningFuturama
 A repository to scrape and analyze the transcripts for Futurama
 
-Inspired by [Kaylin Walker](http://kaylinwalker.com/text-mining-south-park/).
+Inspired by [Text Mining South Park](http://kaylinwalker.com/text-mining-south-park/).
 Transcripts scraped from [The Infosphere, the Futurama Wiki](https://theinfosphere.org/)
--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require("fs");
 
 var baseURL = 'https://theinfosphere.org';
 var directoryURL = '/Episode_Transcript_Listing';
 var urlsToParse = [];
+var responsesReceived = 0;
+
+function Line(character, line, episodeNum){
+    this.character = character;
+    this.line = line;
+    this.episodeNum = episodeNum;
+}
+ var lines = [];
+
 
 request(baseURL + directoryURL, function (error, response, html) {
   if (!error && response.statusCode == 200) {
@@ -28,11 +38,28 @@ function doWork(){
     request(baseURL + urlsToParse[i], function (error, response, html) {
       if (!error && response.statusCode == 200) {
         var $ = cheerio.load(html);
-        $('div.poem').each(function(i, element){
+        $('div.poem').each(function(j, element){
           var a = $(this);
-          console.log(a.text());
+          var text = a.text().split('⨂').pop().split(':');
+          var character = text[0].trim();
+          var characterLine = text[1].replace(/(\[.*?\])/g, '');
+          characterLine = characterLine.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?]/g,"").replace(/\s+/g, " ").trim();
+          console.log(character);
+          console.log(characterLine);
+          lines.push(new Line(character, characterLine, i));
         });
+        responsesReceived++;
+        if (responsesReceived == urlsToParse.length/70){
+            cleanUp();
+        }
       }
     });
   }
+}
+
+function cleanUp(){
+    console.log('meow');
+    for (var i=0; i < lines.length; i++){
+      console.log(lines[i].character + ' ' + lines[i].line + '\n');
+    }
 }
